08cf3a8a69a0289401d5107647f7b5d94039a86c
Hernan Garcia Riffo <mac@macs-MBP-3.home>
2019-03-30 10:11:34 +0100

    test name

--- Presentation/Insurance.WebApi/Startup.cs
+++ Presentation/Insurance.WebApi/Startup.cs
@@ -150,7 +150,7 @@ namespace Insurance.WebApi
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-            // specifying the Swagger JSON endpoint.
+            // specifying the Swagger JSON endpoint. 
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Insurance API V1");
